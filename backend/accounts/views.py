43d9e03d9436234fcd89da7e52f08cecfce7bd68
johnmartinroque <johnmartinroque@gmail.com>
2024-09-25 18:53:29 +0800

    qq

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(['GET'])
+def getText(request):
+    text = [
+        'hello hello'
+    ]
+
+    return Response(text)
