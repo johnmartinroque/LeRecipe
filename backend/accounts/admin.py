7f7dc423e9c3fbcd82897e43574fc8c5b8397e2f
johnmartinroque <johnmartinroque@gmail.com>
2024-09-25 22:51:51 +0800

    a

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+
+    list_display = ['id', 'username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active']
+
+    # Optionally, you can add the user ID to the fields in the detail view
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('id',)}),
+    )
+
+# Unregister the default User admin
+admin.site.unregister(User)
+
+# Register the custom User admin
+admin.site.register(User, CustomUserAdmin)
