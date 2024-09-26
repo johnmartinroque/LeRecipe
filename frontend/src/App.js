865179d000af25cdeb655b1b454519c4ddd4066f
johnmartinroque <johnmartinroque@gmail.com>
2024-09-26 19:26:39 +0800

    protected and catch route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import HomeScreen from './screens/HomeScreen';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import RecipeDetailedScreen from './screens/RecipeDetailedScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import PrivateRoute from './components/PrivateRoute';
+
+
 
 
 
@@ -21,7 +24,8 @@ function App() {
           <Route path='/login' element={<LoginScreen />}  />
           <Route path='/register' element={<RegisterScreen />}  />
           <Route path='/recipe/:id' element={<RecipeDetailedScreen />}  />
-          <Route path='/profile' element={<ProfileScreen />}  />
+          <Route path="*" element={<Navigate to="/" />} />
+          <Route path="/profile" element={<PrivateRoute><ProfileScreen /></PrivateRoute>} />
         </Routes>
         
       </Router>
