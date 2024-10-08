bd1ff9ea25a4d89e89e0363b9ee3aca878716f27
Ranzel <azramirez1@student.hau.edu.ph>
2024-10-09 07:24:53 +0800

    Additional homepage

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import '../css/HomeScreen.css';
+import '../css/screens/HomeScreen.css';
 import MonthRecipe from "../components/MonthRecipe";
 import WeekRecipe from "../components/WeekRecipe";
 import RandomRecipes from "../components/RandomRecipes";
@@ -11,6 +11,20 @@ function HomeScreen() {
     <div>
       <Row>
         <Col>
+        <div className="landing-section">
+          <div className="landing-image">
+            <img src="./LEGOAT.jpg" alt="Delicious Food" />
+          </div>
+          <div className="landing-text">
+            <h1>Welcome to<img 
+            src="/LeRecipe.png" 
+            alt="LeRecipe Logo"/>
+          </h1>
+            <p>Discover amazing recipes and share your culinary creations!</p>
+            <button className="cta-button">Get Started</button>
+          </div>
+        </div>
+
           <RandomRecipes />
           <WeekRecipe />
           <MonthRecipe />
