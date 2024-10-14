f3bcc9663ab74cc4cdc7c7e6582ed133e695ce02
Cain Rivera <conjain30@gmail.com>
2024-10-14 18:28:27 +0800

    About Us fix and forum design

--- frontend/src/screens/ForumDetailed.jsx
+++ frontend/src/screens/ForumDetailed.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
-
+import '../css/screens/ForumDetailed.css';
 import { Alert, Col, Row, Spinner } from "react-bootstrap";
 import { getForumPostDetails } from "../actions/userActions";
 import ForumCommentList from "../components/ForumCommentList";
@@ -21,42 +21,43 @@ function ForumDetailed() {
   }, [dispatch, id]);
 
   return (
-    <div>
+    <div className="forum-detail-container">
       {loading ? (
         <Spinner />
       ) : error ? (
         <Alert variant="danger">{error}</Alert>
       ) : (
-        <Row>
-          <Col>
-            <div style={{ background: "gray" }}>
-              <h1>{forumPost?.title}</h1>
-              <p>{forumPost?.content}</p>
-              {forumPost?.user && (
-                <Link
-                  style={{ textDecoration: "none", color: "white" }}
-                  to={`/user/${forumPost?.user?.id}`}
-                >
-                  {forumPost?.user?.username}
-                </Link>
-              )}
-              {forumPost?.user?.profile_picture && (
-                <img
-                  src={forumPost.user.profile_picture}
-                  alt={`${forumPost?.user?.username}'s profile`}
-                  style={{ width: "100px", borderRadius: "50%" }}
-                />
-              )}
-              <p>
-                Created At:{" "}
-                {forumPost?.created_at
-                  ? new Date(forumPost.created_at).toLocaleString()
-                  : "N/A"}
-              </p>
-            </div>
-            <ForumCommentList postId={id} />
-          </Col>
-        </Row>
+        <div className="forum-detail-content">
+          <div className="forum-detail-box">
+            <h1 className="forum-detail-title">{forumPost?.title}</h1>
+            <p className="forum-detail-content">{forumPost?.content}</p>
+  
+            {/* User info section */}
+            {forumPost?.user && (
+              <div className="user-info">
+                {forumPost?.user?.profile_picture && (
+                  <img
+                    className="forum-user-picture"
+                    src={forumPost.user.profile_picture}
+                    alt={`${forumPost?.user?.username}'s profile`}
+                  />
+                )}
+                <p className="forum-user-link">
+                  <Link to={`/user/${forumPost?.user?.id}`}>
+                    {forumPost?.user?.username}
+                  </Link>
+                </p>
+                <p className="forum-created-at">
+                  Created At:{" "}
+                  {forumPost?.created_at
+                    ? new Date(forumPost.created_at).toLocaleString()
+                    : "N/A"}
+                </p>
+              </div>
+            )}
+          </div>
+          <ForumCommentList postId={id} />
+        </div>
       )}
     </div>
   );
